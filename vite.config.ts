cb8e78cbbf676cb3b4fdffa52ce0b3126ca537db
wimbarelds <wim.barelds@gmail.com>
2025-06-26 20:45:00 +0200

    test with automatic repo name / base

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,13 @@ import { defineConfig, type PluginOption } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
-function baseHrefPlugin(base: string): PluginOption {
-  return {
-    name: 'html-transform',
-    config: (config) => ({ ...config, base }),
-    transformIndexHtml: () => [{ tag: 'base', attrs: { href: base }, injectTo: 'head-prepend' }],
-  };
-}
+const baseHrefPlugin: PluginOption = {
+  name: 'html-basehref',
+  transformIndexHtml: (_, ctx) => [
+    { tag: 'base', attrs: { href: ctx.server?.config.base ?? '/' }, injectTo: 'head-prepend' },
+  ],
+};
 
-export default defineConfig(({ command }) => {
-  if (command === 'build') {
-    return {
-      plugins: [react(), tailwindcss(), baseHrefPlugin('/react-workshop/')],
-    };
-  }
-  return {
-    plugins: [react(), tailwindcss()],
-  };
-});
+export default defineConfig(() => ({
+  plugins: [react(), tailwindcss(), baseHrefPlugin],
+}));
